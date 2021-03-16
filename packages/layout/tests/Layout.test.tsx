0433541237d9f9e23385fb3e2283ae892588fd48
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2021-03-16 12:48:04 -0600

    chore(layout): add a11y tests

--- packages/layout/tests/Layout.test.tsx
+++ packages/layout/tests/Layout.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
 
 import { Grid } from '../src';
 
@@ -7,5 +8,16 @@ describe('Grid', () => {
   it('renders consistently', () => {
     const { container } = render(<Grid />);
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should have no axe violations', async () => {
+    const { container } = render(<Grid />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should have display: grid', () => {
+    const { container } = render(<Grid />);
+    expect(container.firstChild).toHaveStyleRule('display', 'grid');
   });
 });
