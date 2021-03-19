61b5aabe4ce37c21fdb03c3c21257cc245860fc0
Martin Ruiz <martin.ruiz.mares@gmail.com>
2021-03-19 16:45:13 -0600

    feat(box): added flex item properties

--- packages/layout/src/Box.tsx
+++ packages/layout/src/Box.tsx
@@ -8,20 +8,49 @@ import {
   ColorProps,
   LayoutProps,
   FlexProps,
+  flexGrow,
+  FlexGrowProps,
+  flexShrink,
+  FlexShrinkProps,
+  flexBasis,
+  FlexBasisProps,
+  justifySelf,
+  JustifySelfProps,
+  alignSelf,
+  AlignSelfProps,
+  order,
+  OrderProps,
+  compose,
 } from 'styled-system';
 import { ComponentProps } from './utils/types';
+
+const flexItem = compose(
+  flexGrow,
+  flexShrink,
+  flexBasis,
+  justifySelf,
+  alignSelf,
+  order
+);
 
 interface StyledBoxProps
   extends SpaceProps,
     ColorProps,
     LayoutProps,
-    FlexProps {}
+    FlexProps,
+    FlexGrowProps,
+    FlexShrinkProps,
+    FlexBasisProps,
+    JustifySelfProps,
+    AlignSelfProps,
+    OrderProps {}
 
 export const Box = styled.div<StyledBoxProps>`
   ${space}
   ${color}
   ${layout}
   ${flex}
+  ${flexItem}
 `;
 
 export type BoxProps = ComponentProps<typeof Box>;
