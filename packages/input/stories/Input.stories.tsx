5bf28335dfe08a7c0476ce7dee173be0a32c305d
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2021-03-22 15:46:02 -0600

    chore(packages): remove unused button and input packages

--- packages/input/stories/Input.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Input } from '../src';
-
-export default {
-  component: Input,
-  title: 'Design System/Input',
-};
-
-export const primary = () => <Input />;
