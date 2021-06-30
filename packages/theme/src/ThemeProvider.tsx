5f5a2bbef814b333a8d12ccf22e6fcd8c3a4c26e
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2021-06-30 17:56:32 -0500

    chore(theme): fix types and add stories

--- packages/theme/src/ThemeProvider.tsx
+++ packages/theme/src/ThemeProvider.tsx
@@ -5,8 +5,16 @@ import {
 } from 'styled-components';
 import { theme, VitaTheme } from './theme';
 
+// eslint-disable-next-line @typescript-eslint/ban-types
+type SThemeProviderProps<T extends object, U extends object = T> = Omit<
+  StyledThemeProviderProps<T, U>,
+  'theme'
+>;
+
 export interface ThemeProviderProps
-  extends StyledThemeProviderProps<VitaTheme, VitaTheme> {}
+  extends SThemeProviderProps<VitaTheme, VitaTheme> {
+  theme?: VitaTheme | ((t: VitaTheme) => VitaTheme);
+}
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   theme,
--- packages/theme/stories/ThemeProvider.stories.tsx
+++ packages/theme/stories/ThemeProvider.stories.tsx
@@ -1,20 +1,65 @@
 import * as React from 'react';
 import { Meta } from '@storybook/react';
 import styled from 'styled-components';
-import { ThemeProvider } from '../src';
+import { ThemeProvider, getThemeValue } from '../src';
 
 export default {
   component: ThemeProvider,
   title: 'Components/ThemeProvider',
 } as Meta;
 
-const CustomDiv = styled.div<any>`
-  color: ${(props) => props.theme.primaryColor};
-  background: ${(props) => props.theme.backgroundColor};
+const TextPrimary = styled.p`
+  font-family: ${getThemeValue('fonts.primary')};
 `;
 
-export const SimpleFlex = () => (
-  <ThemeProvider theme={{ primaryColor: 'blue', backgroundColor: 'green' }}>
-    <CustomDiv>Hi, I have a custom theme.</CustomDiv>
+const TextSecondary = styled.p`
+  font-family: ${getThemeValue('fonts.secondary')};
+`;
+
+export const Default = () => (
+  <ThemeProvider>
+    <p>I have a default font family</p>
+    <TextPrimary>I have a primary font family</TextPrimary>
+    <TextSecondary>I have a secondary font family</TextSecondary>
+  </ThemeProvider>
+);
+
+export const NoThemeProvider = () => (
+  <div>
+    <p>I have a default font family</p>
+    <TextPrimary>I have a primary font family</TextPrimary>
+    <TextSecondary>I have a secondary font family</TextSecondary>
+  </div>
+);
+
+const customTheme = { fonts: { primary: 'monospace', secondary: 'cursive' } };
+
+export const CustomTheme = () => (
+  <ThemeProvider theme={customTheme}>
+    <p>I have a default font family</p>
+    <TextPrimary>I have a primary font family</TextPrimary>
+    <TextSecondary>I have a secondary font family</TextSecondary>
+  </ThemeProvider>
+);
+
+export const NestedThemes = () => (
+  <ThemeProvider>
+    <h2>Normal Fonts</h2>
+    <TextPrimary>I have a primary font family</TextPrimary>
+    <TextSecondary>I have a secondary font family</TextSecondary>
+    <h2>Inverted fonts</h2>
+    <ThemeProvider
+      theme={(theme) => ({
+        ...theme,
+        fonts: {
+          ...theme.fonts,
+          primary: theme.fonts.secondary,
+          secondary: theme.fonts.primary,
+        },
+      })}
+    >
+      <TextPrimary>Now I have the secondary font</TextPrimary>
+      <TextSecondary>And now I have the primary font</TextSecondary>
+    </ThemeProvider>
   </ThemeProvider>
 );
